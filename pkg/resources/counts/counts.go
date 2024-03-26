e694348881c919fd9c1200934280a880ef5bb591
Bruno Bressi <bruno.bressi@telekom.de>
2024-03-26 18:15:05 +0100

    chore: added changed from https://github.com/rancher/steve/pull/162/files
    Signed-off-by: Bruno Bressi <bruno.bressi@telekom.de>

--- pkg/resources/counts/counts.go
+++ pkg/resources/counts/counts.go
@@ -192,7 +192,7 @@ func (s *Store) Watch(apiOp *types.APIRequest, schema *types.APISchema, w types.
 
 		counts[schema.ID] = itemCount
 		changedCount := map[string]ItemCount{
-			schema.ID: itemCount,
+			schema.ID: *itemCount.DeepCopy(),
 		}
 
 		result <- Count{
